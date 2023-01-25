2ce8502b1ad8a1a5a096ecfccad2dae10e62ec85
miku <i@nightmulti.com>
2023-01-25 21:19:17 +0800

    create user

---
+++ src/main/kotlin/com/nightmulti/music/web/user/User.kt
@@ -0,0 +1,15 @@
+package com.nightmulti.music.web.user
+
+class User {
+
+    var USER_ID = ""
+    var USER_NAME = ""
+    var USER_PHONE = ""
+    var USER_MAIL = ""
+    var USER_TOKEN = ""
+    var USER_PASSWORD = ""
+    var USER_SIGN = ""
+    var USER_BACKGROUND = ""
+    var USER_PROFILE_PHOTO = ""
+
+}
